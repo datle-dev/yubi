c65dba37bb74949d07b2cbedb1e74209f8e000d4
datle-dev <dle.dev@outlook.com>
2024-06-06 13:53:13 -0400

    Initial commit of component for a word
    Create component for a given word, which accepts two arguments: the
    expected word and the word actually typed. Letter components are
    conditionally pushed to an array depending on the differences between
    the expected and typed words, then that array is used to render the word.

---
+++ frontend/src/components/Word.jsx
@@ -0,0 +1,37 @@
+import Letter from "./Letter";
+
+export default function Word({ word, typed }) {
+  const maxLength = Math.max(word.length, typed.length);
+
+  const renderLetters = () => {
+    let letterArray = [];
+
+    for (let i = 0; i < maxLength; i++) {
+      if (word.charAt(i) === "") {
+        letterArray.push(
+          <Letter key={i} letter={typed.charAt(i)} letterType={"extra"} />
+        );
+      } else if (typed.charAt(i) === "") {
+        letterArray.push(
+          <Letter key={i} letter={word.charAt(i)} letterType={"normal"} />
+        );
+      } else if (word.charAt(i) === typed.charAt(i)) {
+        letterArray.push(
+          <Letter key={i} letter={typed.charAt(i)} letterType={"correct"} />
+        );
+      } else {
+        letterArray.push(
+          <Letter key={i} letter={word.charAt(i)} letterType={"wrong"} />
+        );
+      }
+    }
+
+    return letterArray;
+  };
+
+  return (
+    <>
+      <div>{renderLetters()}</div>
+    </>
+  );
+}
