c93649f8d122e469cc6c4699351de9719bc860c9
datle-dev <dle.dev@outlook.com>
2024-06-07 06:29:33 -0400

    Add JetBrainsMono to tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      mono: ['JetBrainsMono-Regular', ...defaultTheme.fontFamily.mono],
+    },
   },
   plugins: [],
 };
