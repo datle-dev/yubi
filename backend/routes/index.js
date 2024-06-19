213386562bea6c94a1907e7e5e5ff0e80cb03390
datle-dev <dle.dev@outlook.com>
2024-06-18 21:56:44 -0400

    Initial commit

---
+++ backend/routes/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import { fileURLToPath } from 'url';
+
+const router = express.Router();
+
+router.get('/available', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../data/available.json'));
+});
+
+router.get('/english-100', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../data/english-100.json'));
+});
+
+router.get('/hololive-en', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../data/hololive-en.json'));
+});
+
+export default router;
