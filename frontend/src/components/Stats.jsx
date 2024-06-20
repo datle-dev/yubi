ee78406869e7f7b42f44397c918538c4b1e45901
datle-dev <dle.dev@outlook.com>
2024-06-20 12:28:43 -0400

    Add tooltips for each statistic with appropriate rounding

--- frontend/src/components/Stats.jsx
+++ frontend/src/components/Stats.jsx
@@ -42,22 +42,63 @@ export default function Stats({
     <>
       <div className="grid grid-cols-2 text-2xl gap-x-6">
         <p className="text-sky-300/50">characters:</p>
-        <p className="text-sky-200">
-          {countTyped}/{errors}/{extras}/{missed}
-        </p>
+        <a
+          data-tooltip-id="character"
+          data-tooltip-content="correct/error/extra/missed"
+        >
+          <p className="text-sky-200">
+            {countTyped}/{errors}/{extras}/{missed}
+          </p>
+        </a>
+        <Tooltip id="character" place="right" />
 
         <p className="text-sky-300/50">raw wpm:</p>
-        <p className="text-sky-200"> {rawWpm}</p>
+        <a
+          data-tooltip-id="rawWpm"
+          data-tooltip-content={`${rawWpm.toFixed(2)} wpm`}
+        >
+          <p className="text-sky-200"> {Math.round(rawWpm)}</p>
+        </a>
+        <Tooltip id="rawWpm" place="right" />
 
         <p className="text-sky-300/50">net wpm:</p>
-        {netWpm > 0 && <p className="text-sky-200">{netWpm}</p>}
-        {netWpm <= 0 && <p className="text-sky-200">invalid</p>}
+        {netWpm > 0 && (
+          <>
+            <a
+              data-tooltip-id="netWpm"
+              data-tooltip-content={`${netWpm.toFixed(2)} wpm`}
+            >
+              <p className="text-sky-200">{Math.round(netWpm)}</p>
+            </a>
+            <Tooltip id="netWpm" place="right" />
+          </>
+        )}
+        {netWpm <= 0 && (
+          <>
+            <a data-tooltip-id="invalid" data-tooltip-content="invalid">
+              <p className="text-sky-200">invalid</p>
+            </a>
+            <Tooltip id="invalid" place="right" />
+          </>
+        )}
 
         <p className="text-sky-300/50">accuracy:</p>
-        <p className="text-sky-200">{percentAccuracy}%</p>
+        <a
+          data-tooltip-id="percentAccuracy"
+          data-tooltip-content={`${percentAccuracy.toFixed(2)}%`}
+        >
+          <p className="text-sky-200">{Math.round(percentAccuracy)}%</p>
+        </a>
+        <Tooltip id="percentAccuracy" place="right" />
 
         <p className="text-sky-300/50">duration:</p>
-        <p className="text-sky-200">{Math.round(duration * 60)}s</p>
+        <a
+          data-tooltip-id="duration"
+          data-tooltip-content={`${(duration * 60).toFixed(2)}s`}
+        >
+          <p className="text-sky-200">{Math.round(duration * 60)}s</p>
+        </a>
+        <Tooltip id="duration" place="right" />
       </div>
     </>
   );
