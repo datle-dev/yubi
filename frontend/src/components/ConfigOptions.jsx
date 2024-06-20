13e37d39e72f8bdf968ddc2885b82f638eb10556
datle-dev <dle.dev@outlook.com>
2024-06-19 20:35:48 -0400

    Update name of mode boolean variables

--- frontend/src/components/ConfigOptions.jsx
+++ frontend/src/components/ConfigOptions.jsx
@@ -4,7 +4,7 @@ import WordCountButton from './buttons/WordCountButton';
 export default function ConfigOptions({ isTimeMode, isWordMode, onClick }) {
   return (
     <>
-      {isWordsTest && (
+      {isWordMode && (
         <div>
           <WordCountButton text={'10'} count={'10'} onClick={onClick} />
           <WordCountButton text={'25'} count={'25'} onClick={onClick} />
@@ -12,7 +12,7 @@ export default function ConfigOptions({ isTimeMode, isWordMode, onClick }) {
           <WordCountButton text={'100'} count={'100'} onClick={onClick} />
         </div>
       )}
-      {isTimedTest && (
+      {isTimeMode && (
         <div>
           <TimeDurationButton text={'15'} duration={'15'} onClick={onClick} />
           <TimeDurationButton text={'30'} duration={'30'} onClick={onClick} />
